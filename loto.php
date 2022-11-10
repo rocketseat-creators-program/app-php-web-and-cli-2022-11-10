f2d279fe3d186090a11122026b96bcc11d5453cb
Sergio Cabral <git@sergiocabral.com>
2022-11-10 01:53:37 -0300

    code: classe de apresentação usando input e output

--- loto.php
+++ loto.php
@@ -17,3 +17,21 @@ abstract class Output implements IOutput { }
 class CliOutput extends Output { }
 
 class WebOutput extends Output { }
+
+
+
+class Presentation {
+    public IInput $input;
+    public IOutput $output;
+
+    function __construct() {
+        $isCli = defined("STDOUT");
+        if ($isCli) {
+            $this->input = new CliInput();
+            $this->output = new CliOutput();
+        } else {
+            $this->input = new WebInput();
+            $this->output = new WebOutput();
+        }
+    }
+}
