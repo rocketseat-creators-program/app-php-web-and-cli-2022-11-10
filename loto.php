c08c437f8643a70bb3ac29a0b24d583680737d77
Sergio Cabral <git@sergiocabral.com>
2022-11-10 02:02:34 -0300

    code: implementado output via cli e web

--- loto.php
+++ loto.php
@@ -10,13 +10,73 @@ class WebInput extends Input { }
 
 
 
-interface IOutput { }
+interface IOutput {
+    function addFields(array $fields): void;
+    function header(): void;
+    function body(array $results): void;
+    function footer(): void;
+}
 
-abstract class Output implements IOutput { }
+abstract class Output implements IOutput {
+    protected array $fields = [];
 
-class CliOutput extends Output { }
+    function addFields(array $fields): void {
+        $this->fields = array_merge($this->fields, $fields);
+    }
+}
 
-class WebOutput extends Output { }
+class CliOutput extends Output {
+    function header(): void {
+        echo "{$this->fields['title']}" . PHP_EOL;
+        echo str_repeat('^', strlen($this->fields['title'])) . PHP_EOL;
+        ECHO PHP_EOL;
+    }
+
+    function body(array $results): void {
+        echo "RESULTADOS DA CONSULTA:" . PHP_EOL;
+        echo PHP_EOL;
+        foreach ($results as $label => $value) {
+            echo " | " . $label . ": " . PHP_EOL;
+            echo " +--> " . $value . PHP_EOL;
+            echo PHP_EOL;
+        }
+    }
+
+    function footer(): void {
+        echo "Script execution finished." . PHP_EOL;
+        echo PHP_EOL;
+    }
+}
+
+class WebOutput extends Output {
+    function header(): void {
+        echo "<!DOCTYPE html>";
+        echo "<html lang='en'>";
+        echo "<head>";
+        echo "<meta charset='UTF-8'>";
+        echo "<title>{$this->fields['title']}</title>";
+        echo "</head>";
+        echo "<body>";
+        echo "<h1>{$this->fields['title']}</title></h1>";
+    }
+
+    function body(array $results): void {
+        echo "<div class='results'>";
+        echo "<h2>Resultado da consulta</h2>";
+        foreach ($results as $label => $value) {
+            echo "<div class='result'>";
+            echo "<span class='label'>{$label}</span>";
+            echo "<span class='value'>{$value}</span>";
+            echo "</div>";
+        }
+        echo "</div>";
+    }
+
+    function footer(): void {
+        echo "</body>";
+        echo "</html>";
+    }
+}
 
 
 
@@ -35,3 +95,18 @@ class Presentation {
         }
     }
 }
+
+
+
+$presentation = new Presentation();
+
+$presentation->output->addFields(["title" => "LotoQuery"]);
+$presentation->output->header();
+
+$data = [
+    "chave1" => "valor1",
+    "chave2" => "valor2",
+];
+
+$presentation->output->body($data);
+$presentation->output->footer();
